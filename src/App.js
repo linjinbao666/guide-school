82fa7485f095ec7c82bfac51a992d8cb84c4b436
linjinbao666 <linjinbao666@gmail.com>
2023-06-20 20:27:24 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { Modal, Tabs, Typography, Carousel } from 'antd';
+const { TabPane } = Tabs;
+const { Text } = Typography;
+
 
 const App = () => {
   // 创建坐标数组
@@ -14,12 +18,43 @@ const App = () => {
 
   // 定义包含地图图标信息的数组
   const mapIconCells = [
-    { row: 2, col: 4, title: '标题1', content: 'Some text' },
-    { row: 5, col: 9, title: '标题2',content: ['image1.jpg', 'image2.jpg'] },
-    { row: 10, col: 2, title: '标题3', content: 'video.mp4' },
+    { row: 2, col: 4, title: '标题1', text: 'Some Text', images: ['/images/3.jpg'], audio: '', video: 'a.mp4' },
+    { row: 5, col: 9, title: '标题2', text: 'Some Text2', images: ['a.jpg'], audio: '', video: 'b.mp4' },
+    { row: 10, col: 2, title: '标题3', text: 'Some Text3', images: ['a.jpg'], audio: '', video: 'c.mp4' },
   ];
 
-  
+  const [dialogTitle, setDialogTitle] = useState('');
+  const [dialogText, setDialogText] = useState(null);
+  const [dialogImages, setDialogImages] = useState(null);
+  const [dialogaudio, setDialogaudio] = useState(null);
+  const [dialogvideo, setDialogvideo] = useState(null);
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [activeTab, setActiveTab] = useState('text');
+
+  const handleGridCellClick = (title, text, images, audio, video) => {
+    setDialogTitle(title);
+    setDialogText(text);
+    console.log(text);
+    setDialogImages(images);
+    console.log(images)
+    setDialogaudio(audio);
+    console.log(audio)
+    setDialogvideo(video);
+    setDialogVisible(true);
+  };
+
+  const closeDialog = () => {
+    setDialogTitle('');
+    setDialogText(null);
+    setDialogImages(null);
+    setDialogaudio(null);
+    setDialogvideo(null);
+    setDialogVisible(false);
+  };
+
+  const handleTabChange = (key) => {
+    setActiveTab(key);
+  };
 
   return (
     <div className="container">
@@ -31,13 +66,17 @@ const App = () => {
             cell => cell.row === coord.row && cell.col === coord.col
           );
           const isMapIconCell = !!mapIconCell;
-          const content = isMapIconCell ? mapIconCell.content : null;
+          const title = isMapIconCell ? mapIconCell.title : null;
+          const text = isMapIconCell ? mapIconCell.text : null;
+          const images = isMapIconCell ? mapIconCell.images : null;
+          const audio = isMapIconCell ? mapIconCell.audio : null;
+          const video = isMapIconCell ? mapIconCell.video : null;
           return (
             <div className="grid-cell" key={index}>
               {isMapIconCell ? (
                 <div
                   className="grid-icon"
-                  
+                  onClick={() => handleGridCellClick(title, text, images, audio, video)}
                 >
                   <FontAwesomeIcon icon={faMapMarkerAlt} className="map-icon" />
                 </div>
@@ -49,6 +88,41 @@ const App = () => {
           );
         })}
       </div>
+      <Modal
+        title={<div style={{ textAlign: 'center' }}>{dialogTitle}</div>}
+        visible={dialogVisible}
+        onCancel={closeDialog}
+        footer={null}
+        centered
+      >
+        <Tabs
+          type="card"
+          accessKey={activeTab}
+          tabBarStyle={{ justifyContent: 'center' }}
+          onChange={handleTabChange}
+          centered
+          style={{ height: '400px' }} >
+          <TabPane tab="Text" key="text">
+            <Text>{dialogText}</Text>
+          </TabPane>
+          <TabPane tab="Image" key="image">
+            <Carousel autoplay>
+              {dialogImages && dialogImages.map((image, index) => (
+                <div key={index}>
+                  <img src={image} alt={`Image ${index + 1}`} className="carousel-image" />
+                </div>
+              ))}
+            </Carousel>
+          </TabPane>
+          <TabPane tab="Audio" key="audio">
+            <audio src={dialogaudio} controls width="100%" />
+          </TabPane>
+          <TabPane tab="Video" key="video">
+            <video src={dialogvideo} controls width="100%" />
+          </TabPane>
+
+        </Tabs>
+      </Modal>
     </div>
   );
 }
